ae7ee205d346b0f3f1efebbccf1d7e176e8d34c2
Kevin Lewis <kevin.lewis@vonage.com>
2020-05-18 22:50:36 +0100

    Works

--- functions/src/answer.js
+++ functions/src/answer.js
@@ -1,3 +1,14 @@
+require('dotenv').config()
+const { AIRTABLE_BASE, AIRTABLE_KEY, AIRTABLE_TABLE } = process.env
+
+const Airtable = require('airtable-plus')
+const base = new Airtable({
+    baseID: AIRTABLE_BASE,
+    apiKey: AIRTABLE_KEY,
+    tableName: AIRTABLE_TABLE
+})
+
+
 const headers = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Headers': 'Content-Type'
@@ -5,8 +16,22 @@ const headers = {
 
 exports.handler = async (event, context) => {
     try {
-        return { headers, statusCode: 200, body: 'ok' }
+        const thoughts = await base.read({ filterByFormula: '{Today}="Yes"' })
+        const ncco = [];
+        if(thoughts.length > 0) {
+            ncco.push({
+                action: 'stream',
+                streamUrl: [thoughts[0].fields.File[0].url]
+            })
+        } else {
+            ncco.push({
+                action: 'talk',
+                voiceName: 'Brian',
+                text: 'Sorry I haven\'t got around to recording today\'s thoguth of the day yet. God bless.'
+            })
+        }
+        return { headers, statusCode: 200, body: JSON.stringify(ncco) }
     } catch(err) {
-        return { headers, statusCode: 500, body: 'Error: ' + e }
+        return { headers, statusCode: 500, body: 'Error: ' + err }
     }
 }
